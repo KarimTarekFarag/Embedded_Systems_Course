4dd892d6a58492ba06d1a78bca62e16bc0a380a6
Karim <karim.t.farag@gmail.com>
2019-12-27 01:10:36 +0200

    2612

--- Lecture files/test.c
+++ Lecture files/test.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
-#include "LIB_StdTypes.h"
-
-#define x 40
-
-
-int main(void){
-#if x>10 
-printf("Ahmed");
-#elif x<10
-printf("Gemy");
-#endif
+#include <stdlib.h> //used for malloc & free functions
+int main(){
+int n,i,*ptr,sum=0;
+printf("Enter number of elements: ");
+scanf("%d",&n);
+ptr=(int*)malloc(n*sizeof(int)); //memory allocated using malloc
+////assert(ptr==NULL); //check if memory is allocated or not
+printf("Enter elements of array: ");
+for(i=0;i<n;++i) {
+scanf("%d",ptr+i);
+sum+=*(ptr+i); }
+printf("Sum=%d",sum);
+free(ptr); //delete the dynamic allocated memory
 return 0;
 }
