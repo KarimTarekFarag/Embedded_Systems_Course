8dbb03fa850baf483240a54e979f9cc45825d028
Karim <karim.t.farag@gmail.com>
2019-12-28 00:11:47 +0200

    es

--- Lecture files/test.c
+++ Lecture files/test.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h> //used for malloc & free functions
-int main(){
-int n,i,*ptr,sum=0;
-printf("Enter number of elements: ");
-scanf("%d",&n);
-ptr=(int*)malloc(n*sizeof(int)); //memory allocated using malloc
-////assert(ptr==NULL); //check if memory is allocated or not
-printf("Enter elements of array: ");
-for(i=0;i<n;++i) {
-scanf("%d",ptr+i);
-sum+=*(ptr+i); }
-printf("Sum=%d",sum);
-free(ptr); //delete the dynamic allocated memory
-return 0;
+
+#include <stdlib.h>
+
+int main()
+
+{
+
+int x = 6;
+
+while(x--)
+
+{
+
+if(x == 5)
+
+{
+
+printf("Hello Sprints, Yes\n");
+
+}
+
+else
+
+{
+
+printf("Hello Sprints, No\n");
+
+}
+
+}
+
 }
